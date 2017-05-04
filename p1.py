86d0bd5068b4c1593b0e731ddc255ce1249f90b0
mgent83 <mgent83@hotmail.com>
2017-05-04 15:52:45 -0400

    changed structure so that p1 connects directly to p2

--- p1.py
+++ p1.py
@@ -1,46 +1,29 @@
 # Player 1 Program
 
-from twisted.internet.protocol import ClientFactory
+from twisted.internet.protocol import Factory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 from twisted.internet.defer import DeferredQueue
+import time
 
-
-# INIT CONNECTION
-class InitConnection(Protocol):
+# PLAYER CONNECTION
+class PlayerConnection(Protocol):
 	def connectionMade(self):
-		print("init connection made")
-
-	def dataReceived(self, data):
-		if (data == 'send p1'):
-			reactor.connectTCP("ash.campus.nd.edu", 42402, Player1ConnectionFactory())
-
-class InitConnectionFactory(ClientFactory):
-	def __init__(self):
-		self.initCon = InitConnection()
-
-	def buildProtocol(self, addr):
-		return self.initCon
-
-# PLAYER 1 CONNECTION
-class Player1Connection(Protocol):
-	def connectionMade(self):
-		# Create service connection
+		# Create player connection
 		print("player 1 connection made")
 	
 	def dataReceived(self, data):
-		# server.py has sent data to player 1: update game
-		print("data sent to player1: ", data)
+		# server.py has sent data to player 2: update game
+		print("data received from player2: ", data)
 
-class Player1ConnectionFactory(ClientFactory):
+class PlayerConnectionFactory(Factory):
 	def __init__(self):
-		self.p1Con = Player1Connection()
+		self.p1Con = PlayerConnection()
 
 	def buildProtocol(self, addr):
 		return self.p1Con
 
-if __name__ == "__main__":
-	reactor.connectTCP("ash.campus.nd.edu", 40402, InitConnectionFactory())
-	reactor.run()
+reactor.listenTCP(40402, PlayerConnectionFactory())
+reactor.run()
 
 
--- p2.py
+++ p2.py
@@ -1,28 +1,30 @@
-# Player 1 Program
+# Player 2 Program
 
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 from twisted.internet.defer import DeferredQueue
+from twisted.protocols.basic import LineReceiver
 
 # PLAYER 2 CONNECTION
-class Player2Connection(Protocol):
+class PlayerConnection(Protocol):
 	def connectionMade(self):
-		# Create player 2 connection
+		# Create player connection
 		print("player 2 connection made")
-	
+		
 	def dataReceived(self, data):
-		# server.py has sent data to player 2: update game
-		print("data sent to player2: ", data)
+		# server.py has sent data to player 1: update game
+		print("data received from player1: ", data)
 
-class Player2ConnectionFactory(ClientFactory):
+class PlayerConnectionFactory(ClientFactory):
 	def __init__(self):
-		self.p2Con = Player2Connection()
+		self.p2Con = PlayerConnection()
 
 	def buildProtocol(self, addr):
 		return self.p2Con
 
-reactor.connectTCP("ash.campus.nd.edu", 41402, Player2ConnectionFactory())
-reactor.run()
 
+if __name__ == "__main__":
+	reactor.connectTCP("ash.campus.nd.edu", 40402, PlayerConnectionFactory())
+	reactor.run()
 
