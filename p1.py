6dbb9ad02fa6ebb5250f71eb453c1ec8f02042fd
mgent83 <mgent83@hotmail.com>
2017-05-05 16:18:09 -0400

    players communicate, game window in progress

--- p1.py
+++ p1.py
@@ -3,27 +3,101 @@
 from twisted.internet.protocol import Factory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
-from twisted.internet.defer import DeferredQueue
-import time
+import sys, os, pygame, math
+from pygame.locals import *
+from twisted.internet.task import LoopingCall
+
+# Loads images
+def load_image(name):
+	image = pygame.image.load(name)
+	image = image.convert()
+	return image, image.get_rect()
+
+# GAMESPACE
+class GameSpace():
+	def __init__(self, p1Con):
+		# Add network connection
+		self.p1Con = p1Con
+
+		# Initialize window settings
+		pygame.init()
+		pygame.key.set_repeat(100, 30)
+		self.back = 30, 144, 255 
+		self.size = self.width, self.height = 640, 480
+		self.screen = pygame.display.set_mode(self.size)
+		
+		# Initialize game objects
+		self.clock = pygame.time.Clock()
+		self.background = Background(self)
+		self.myAvatar = Avatar(self)
+		self.sprites = [self.background, self.myAvatar] # contains list of objects
+		self.allsprites = pygame.sprite.RenderPlain(self.sprites)
+		
+	def loop(self):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				self.p1Con.transport.write("button pressed")
+		for sprite in self.sprites:
+			sprite.tick()
+
+		# Update screen
+		self.allsprites.update()
+		self.screen.fill(self.back)
+		for i in self.sprites:
+			self.screen.blit(i.image, i.rect)
+		self.allsprites.draw(self.screen)
+		pygame.display.flip()
+		
+# BACKGROUND
+class Background(pygame.sprite.Sprite):
+	def __init__(self, gs):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('images/clouds.jpg')
+		self.rect.topleft = 0, 0
+	def tick(self):
+		i = 1
+
+# AVATAR CLASS
+class Avatar(pygame.sprite.Sprite):
+	def __init__(self, gs):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('images/globe.png')
+		self.rect.topleft = 10, 10
+
+	def tick(self):
+		i = 4
 
 # PLAYER CONNECTION
 class PlayerConnection(Protocol):
 	def connectionMade(self):
 		# Create player connection
+		game = GameSpace(p1Con.getConnection())
+		lc = LoopingCall(game.loop)
+		lc.start(1/60)
 		print("player 1 connection made")
 	
 	def dataReceived(self, data):
 		# server.py has sent data to player 2: update game
 		print("data received from player2: ", data)
+		
 
 class PlayerConnectionFactory(Factory):
 	def __init__(self):
 		self.p1Con = PlayerConnection()
+	
+	def getConnection(self):
+		return self.p1Con
 
 	def buildProtocol(self, addr):
 		return self.p1Con
 
-reactor.listenTCP(40402, PlayerConnectionFactory())
-reactor.run()
+
+if __name__ == '__main__':
+	p1Con = PlayerConnectionFactory()
+	reactor.listenTCP(40402, p1Con)
+	reactor.run()
 
 
--- p2.py
+++ p2.py
@@ -3,15 +3,72 @@
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
-from twisted.internet.defer import DeferredQueue
-from twisted.protocols.basic import LineReceiver
+import sys, os, pygame, math
+from pygame.locals import *
+from twisted.internet.task import LoopingCall
+
+
+# Function that loads images
+def load_image(name):
+	image = pygame.image.load(name)
+	image = image.convert()
+	return image, image.get_rect()
+
+# GAMESPACE
+class GameSpace():
+	def __init__(self, p2Con):
+		# Add network connection
+		self.p2Con = p2Con
+
+		# Initialize window settings
+		pygame.init()
+		pygame.key.set_repeat(100, 30)
+		self.black = 30, 144, 255
+		self.size = self.width, self.height = 640, 480
+		self.screen = pygame.display.set_mode(self.size)
+
+		# Initialize game objects
+		self.clock = pygame.time.Clock()
+		self.myAvatar = Avatar(self)
+		self.sprites = [self.myAvatar]
+		self.allsprites = pygame.sprite.RenderPlain(self.sprites)
+
+	def loop(self):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit
+		for sprite in self.sprites:
+			sprite.tick()
+
+		# Update screen
+		self.allsprites.update()
+		self.screen.fill(self.black)
+		for i in self.sprites:
+			self.screen.blit(i.image, i.rect)
+		self.allsprites.draw(self.screen)
+		pygame.display.flip
+
+# AVATAR CLASS
+class Avatar(pygame.sprite.Sprite):
+	def __init__(self, gs):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('images/globe.png')
+		self.rect.topleft = 10, 10
+
+	def tick(self):
+		i = 5
+
 
 # PLAYER 2 CONNECTION
 class PlayerConnection(Protocol):
 	def connectionMade(self):
 		# Create player connection
 		print("player 2 connection made")
-		
+		game = GameSpace(p2Con.getConnection())
+		lc = LoopingCall(game.loop)
+		lc.start(1/60)
+
 	def dataReceived(self, data):
 		# server.py has sent data to player 1: update game
 		print("data received from player1: ", data)
@@ -19,12 +76,16 @@ class PlayerConnection(Protocol):
 class PlayerConnectionFactory(ClientFactory):
 	def __init__(self):
 		self.p2Con = PlayerConnection()
+	
+	def getConnection(self):
+		return self.p2Con
 
 	def buildProtocol(self, addr):
 		return self.p2Con
 
 
 if __name__ == "__main__":
-	reactor.connectTCP("ash.campus.nd.edu", 40402, PlayerConnectionFactory())
+	p2Con = PlayerConnectionFactory()
+	reactor.connectTCP("ash.campus.nd.edu", 40402, p2Con)
 	reactor.run()
 
